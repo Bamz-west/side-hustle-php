dbec7551c2077d3557c3a5f19a8434a8a22b620c
Fabamise Adeolu <fabamiseadeolu@gmail.com>
2021-06-27 23:51:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+  function range($start, $end) {
+    echo range($start, $end);
+  }
+  
+  echo range(1, 7);
+
+  function sum($num1, $num2) {
+    echo $num1 + $num2;
+  }
+  
+  echo sum(1, 2);
+
+?>
